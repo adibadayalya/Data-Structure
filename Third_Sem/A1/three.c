acc49c5a822f5f2e8a8e71965dc97fc0771bafee
adibadayalya <64812108+adibadayalya@users.noreply.github.com>
2020-08-31 20:35:56 +0530

    Update three.c

--- Third_Sem/A1/three.c
+++ Third_Sem/A1/three.c
@@ -1,9 +1,15 @@
+//question 3
 #include<stdio.h>
 int main()
 {
-	long long a,b;
-	scanf("%lld%lld",&a,&b);
-	printf("%lld\n",a+b);
-	printf("%lld\n",a-b);
-	return 0;
+  long long a,b;
+  char c;
+  scanf("%lli",&a);
+  getchar();
+  scanf("%s",&c);
+  scanf("%lli",&b);
+  if(c=='+')
+  printf("%lli",a+b);
+  else printf("%lli",a-b);
+  return 0;
 }
